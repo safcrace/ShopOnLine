0b8d309e07f16e4a4f30b22dde8c890ab83b0e80
Sender Flores <safcrace@gmail.com>
2020-08-10 20:37:05 -0600

    API Architecture Final Implementation

---
+++ Infrastructure/Data/ProductRepository.cs
@@ -0,0 +1,43 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Core.Entities;
+using Core.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly StoreContext _dbcontext;
+        public ProductRepository(StoreContext dbcontext)
+        {
+            _dbcontext = dbcontext;
+        }
+
+        public async Task<IReadOnlyList<Product>> GetListProductAsync()
+        {
+            return await _dbcontext.Products
+                .Include(p => p.ProductType)
+                .Include(p => p.ProductBrand)
+                .ToListAsync();
+        }
+
+        public async Task<IReadOnlyList<ProductBrand>> GetProductBrandsAsync()
+        {
+            return await _dbcontext.ProductBrands.ToListAsync();
+        }
+
+        public async Task<Product> GetProductByIdAsync(int id)
+        {
+            return await _dbcontext.Products
+                .Include(p => p.ProductType)
+                .Include(p => p.ProductBrand)
+                .FirstOrDefaultAsync(p => p.Id == id);
+        }
+
+        public async Task<IReadOnlyList<ProductType>> GetProductTypesAsync()
+        {
+             return await _dbcontext.ProductTypes.ToListAsync();
+        }
+    }
+}
